cb0a4081def7935249445598c932cd27d43303dc
sheritsh <sheritsh@student.21-school.ru>
2023-07-05 21:31:55 +0300

    feat: all except highlighting effects

--- rs-css/src/app/app.ts
+++ rs-css/src/app/app.ts
@@ -21,12 +21,13 @@ export default class App {
   this.cssDisplayChildren = [];
   this.htmlDisplayChildren = [];
   this.navDisplayChildren = [];
+  this.getCurrentLvl();
   this.composeEventDisplay();
   this.composeCssDisplay();
   this.composeHtmlDisplay();
   this.composeNavDisplay();
   this.createView(this.cssDisplayChildren, this.htmlDisplayChildren, this. navDisplayChildren, this.eventDisplayChildren);
-  }
+}
 
   createView(cssDisplayChildren: HTMLElement[], htmlDisplayChildren: HTMLElement[], navDisplayChildren: HTMLElement[], eventDisplayChildren: HTMLElement[]): void {
     const main = new MainView();
@@ -48,6 +49,11 @@ export default class App {
     document.querySelector('.main')?.appendChild(leftSideContainer);
     rightSideContainer.append(navDispay.getHtmlElement());
     document.querySelector('.main')?.appendChild(rightSideContainer);
+    const winnerWindow = document.createElement('div');
+    winnerWindow.classList.add('winner-window', 'none');
+    winnerWindow.textContent = 'Поздравляю! Ты победил!';
+    document.body.appendChild(winnerWindow);
+    this.receiveEnteredSelector();
   }
 
   composeCssDisplay(): void {
@@ -68,14 +74,41 @@ export default class App {
     for (let i = 0; i < 30; i++) {
       lineNumbers.innerHTML += `${i+1}<br>`;
     }
+    const inputArea = document.createElement('div');
+    inputArea.classList.add('input-area');
+    const formCss = document.createElement('form');
+    formCss.id = 'selector-form';
     const inputCss = document.createElement('input');
+    inputCss.innerHTML = '<pre><code>'
     inputCss.setAttribute('type', 'text');
     inputCss.setAttribute('placeholder', 'Type in a CSS selector');
+    inputCss.setAttribute('maxlength', '20');
     inputCss.classList.add('input-css');
+    inputCss.id = 'selector-input'
+    const sendBtn = document.createElement('button');
+    sendBtn.textContent = 'Enter';
+    sendBtn.setAttribute('type', 'submit');
     const description = document.createElement('div');
     description.innerHTML = `<br>{<br> /* Styles would go here.<br>*/<br>}`;
     description.classList.add('description');
-    editorArea.append(lineNumbers, inputCss, description);
+    const helpBtn = document.createElement('button');
+    helpBtn.textContent = 'Help';
+    helpBtn.addEventListener('click', () => {
+      this.setLvlHelpedStatus();
+      inputCss.classList.add('typewritten');
+      let index = 0;
+      const typingInterval = setInterval(() => {
+        inputCss.value += levelsData[this.currentLevel - 1].selector.charAt(index);
+        index++;
+        if (index >= levelsData[this.currentLevel - 1].selector.length) {
+          clearInterval(typingInterval);
+        }
+      }, 200);
+      inputCss.classList.remove('typewritten');
+    });
+    formCss.append(inputCss, sendBtn);
+    inputArea.append(formCss, helpBtn);
+    editorArea.append(lineNumbers, inputArea, description);
     editorPanel.append(inputHeader, editorArea);
     this.cssDisplayChildren.push(editorPanel);
   }
@@ -99,8 +132,10 @@ export default class App {
       lineNumbers.innerHTML += `${i+1}<br>`;
     }
     const inputHtml = document.createElement('div');
-    inputHtml.textContent = `<div style="castle"> \r\n</div>`;
-    inputHtml.style.whiteSpace = 'pre';
+    inputHtml.textContent += `<div style="castle">`;
+    inputHtml.textContent += levelsData[this.currentLevel - 1].htmlMarkup;
+    inputHtml.textContent += `\n</div>`;
+    inputHtml.style.whiteSpace = 'pre-line';
     inputHtml.classList.add('input-html');
     editorArea.append(lineNumbers, inputHtml);
     editorPanel.append(inputHeader, editorArea);
@@ -108,12 +143,45 @@ export default class App {
   }
 
   composeEventDisplay() {
-    const princess = document.createElement('div');
-    princess.classList.add('princess');
-    this.eventDisplayChildren.push(princess);
+    const eventScreen = document.createElement('div');
+    eventScreen.classList.add('obj-screen');
+    eventScreen.innerHTML = levelsData[this.currentLevel - 1].htmlMarkup;
+    this.eventDisplayChildren.push(eventScreen);
   }
 
   composeNavDisplay(){
+    const navHeader = document.createElement('div');
+    navHeader.classList.add('nav__header');
+    const curLvl = document.createElement('div');
+    curLvl.classList.add('nav__header_cur-lvl');
+    curLvl.textContent = `Level ${this.currentLevel} of ${levelsData.length}`;
+    const curLvlStatus = document.createElement('div');
+    curLvlStatus.classList.add('level-status');
+    const savedStatus = localStorage.getItem(`lvl${this.currentLevel}`);
+    if (savedStatus === 'complete') {
+      curLvlStatus.classList.add('status-green');
+    } else if (savedStatus === 'helped') {
+      curLvlStatus.classList.add('status-orange');
+    } else {
+      curLvlStatus.classList.add('status-red');
+    }
+    const prevLvl = document.createElement('div');
+    prevLvl.classList.add('nav__header_prev');
+    prevLvl.addEventListener('click', () => {
+      this.prevLvl();
+    });
+    const nxtLvl = document.createElement('div');
+    nxtLvl.addEventListener('click', () => {
+      this.nextLvl();
+    });
+    nxtLvl.classList.add('nav__header_next');
+    const lvlMenu = document.createElement('div');
+    lvlMenu.classList.add('nav__header-menu');
+    lvlMenu.addEventListener('click', () => {
+      levelSwitchMenu.classList.remove('translated');
+    });
+    navHeader.append(curLvl, curLvlStatus, prevLvl, nxtLvl, lvlMenu);
+
     const title = document.createElement('div');
     title.classList.add('nav__title');
     title.textContent = levelsData[this.currentLevel - 1].title;
@@ -139,7 +207,146 @@ export default class App {
       }
     });
 
-    this.navDisplayChildren.push(title, toDo, syntax, help, examplesTitle, examples);
+    const levelSwitchMenu = document.createElement('div');
+    levelSwitchMenu.classList.add('nav__level-switch', 'translated');
+    const levelMenuHeader = document.createElement('div');
+    levelMenuHeader.classList.add('level-switch__header');
+    const levelTitle = document.createElement('div');
+    levelTitle.classList.add('level-switch__title');
+    levelTitle.textContent = 'Choose a level';
+    const closeBtn = document.createElement('div');
+    closeBtn.classList.add('level-switch__close-btn');
+    closeBtn.addEventListener('click', () => {
+      levelSwitchMenu.classList.add('translated');
+    });
+    const levelsBar =  document.createElement('div');
+    levelsBar.classList.add('level-switch__levels');
+    const savedStatuses: unknown[] = [];
+    for(let i = 0; i < levelsData.length; i++) {
+      savedStatuses.push(localStorage.getItem(`lvl${i}`));
+    }
+    levelsData.forEach((level, index) => {
+      let lvlColor: string;
+      const levelLine = document.createElement('div');
+      levelLine.classList.add("nav__level-line");
+      if (this.currentLevel === index+1) {
+        levelLine.classList.add("current");
+      }
+      if (savedStatuses[index+1] === 'complete') {
+        lvlColor = 'status-green';
+      } else if (savedStatuses[index+1] === 'helped') {
+        lvlColor = 'status-orange';
+      } else {
+        lvlColor = 'status-red';
+      }
+      levelLine.innerHTML += `<div class="level-status ${lvlColor}"></div>`;
+      levelLine.innerHTML += `<div class ="level-numb">${level.level}</div>`;
+      levelLine.innerHTML += `<div>${level.syntax}</div>`;
+      levelLine.addEventListener('click', () => {
+        this.currentLevel = level.level;
+        this.refresh();
+      });
+      levelsBar.append(levelLine);
+    });
+    const resetBtn = document.createElement('button');
+    resetBtn.classList.add('reset-btn');
+    resetBtn.textContent = 'Reset Progress';
+    resetBtn.addEventListener('click', () => {
+      localStorage.clear();
+      this.currentLevel = 1;
+      this.refresh();
+    });
+    levelMenuHeader.append(levelTitle, closeBtn);
+    levelSwitchMenu.append(levelMenuHeader, levelsBar, resetBtn);
+    this.navDisplayChildren.push(navHeader ,title, toDo, syntax, help, examplesTitle, examples, levelSwitchMenu);
   }
 
+  nextLvl(): void {
+    if (this.currentLevel < 10) {
+      this.currentLevel++;
+      this.refresh();
+    }
+  }
+
+  prevLvl() {
+    if (this.currentLevel > 1) {
+      this.currentLevel--;
+      this.refresh();
+    }
+  }
+
+  refresh() {
+    document.body.innerHTML = '';
+    this.eventDisplayChildren = [];
+    this.cssDisplayChildren = [];
+    this.htmlDisplayChildren = [];
+    this.navDisplayChildren = [];
+    this.composeEventDisplay();
+    this.composeCssDisplay();
+    this.composeHtmlDisplay();
+    this.composeNavDisplay();
+    this.createView(this.cssDisplayChildren, this.htmlDisplayChildren, this. navDisplayChildren, this.eventDisplayChildren);
+  }
+
+  getCurrentLvl() {
+    const lvlFromStorage = Number(localStorage.getItem('currentLvl'));
+    if (lvlFromStorage > 0) {
+      this.currentLevel = lvlFromStorage;
+    }
+  }
+
+  setLvlCompleteStatus() {
+    if (localStorage.getItem(`lvl${this.currentLevel}`) !== 'helped') {
+      localStorage.setItem(`lvl${this.currentLevel}`, 'complete');
+    }
+  }
+
+  setLvlHelpedStatus() {
+    if (localStorage.getItem(`lvl${this.currentLevel}`) !== 'complete') {
+      localStorage.setItem(`lvl${this.currentLevel}`, 'helped');
+    }
+  }
+
+  receiveEnteredSelector() {
+    document.getElementById('selector-form')?.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const inputSelector = document.getElementById('selector-input') as HTMLInputElement;
+      const correctObjects = document.querySelectorAll(`.obj-screen ${levelsData[this.currentLevel - 1].selector}`);
+      if (inputSelector.value === levelsData[this.currentLevel - 1].selector) {
+        this.setLvlCompleteStatus();
+        correctObjects.forEach((obj) => {
+          obj?.classList.add('gelatine');
+        });
+        setTimeout(() => {
+          correctObjects.forEach((obj) => {
+            obj?.classList.remove('gelatine');
+          });
+          if (this.currentLevel === 10) {
+            document.querySelector('.winner-window')?.classList.remove('none');
+          } else {
+            this.nextLvl();
+            this.refresh();
+          }
+        }, 1000);
+      } else {
+        const selectedObjects = document.querySelectorAll(`.obj-screen ${inputSelector.value}`);
+        if (selectedObjects.length > 0) {
+          selectedObjects.forEach((obj) => {
+            obj?.classList.add('shake');
+          });
+          setTimeout(() => {
+            selectedObjects.forEach((obj) => {
+              obj?.classList.remove('shake');
+            });
+          }, 500);
+        } else {
+          const gamingArea = document.querySelector('.gaming-area');
+          gamingArea?.classList.add('shake');
+          setTimeout(() => {
+            gamingArea?.classList.remove("shake");
+          }, 500);
+        }
+      }
+    });
+  }
 }
--- rs-css/src/app/data/levels.ts
+++ rs-css/src/app/data/levels.ts
@@ -4,7 +4,22 @@ const levelsData: ILevelData[] = [
   {
     level: 1,
     title: 'Type Selector',
-    toDo: 'Select elements by their type',
+    toDo: 'Select the princess',
+    selector: 'princess',
+    syntax: 'A',
+    help:
+    'Selects all elements of type <strong>A</strong>. Type refers to the type of tag, so <tag>div</tag>, <tag>p</tag> and <tag>ul</tag> are all different element types.',
+    examples: [
+      '<strong>div</strong> selects all <tag>div</tag> elements.',
+      '<strong>p</strong> selects all <tag>p</tag> elements.',
+    ],
+    htmlMarkup: `
+    <princess></princess>`
+  },
+  {
+    level: 2,
+    title: 'Type Selector',
+    toDo: 'Select the all knights',
     selector: 'knight',
     syntax: 'A',
     help:
@@ -16,9 +31,159 @@ const levelsData: ILevelData[] = [
     htmlMarkup: `
     <knight></knight>
     <princess></princess>
-    <knight></knight>
-    `
+    <knight></knight>`
   },
+  {
+    level: 3,
+    title: 'ID Selector',
+    toDo: 'Select the aggressive knight',
+    selector: '#aggressive',
+    syntax: '#id',
+    help:
+    'Selects the element with a specific <strong>id</strong>. You can also combine the ID selector with the type selector.',
+    examples: [
+      '<strong>#cool</strong> selects any element with <strong>id="cool"</strong>',
+      '<strong>ul#long</strong> selects <tag>ul id="long"</tag>',
+    ],
+    htmlMarkup: `
+    <knight id="aggressive"></knight>
+    <princess></princess>
+    <knight></knight>`
+  },
+  {
+    level: 4,
+    title: 'Descendant Selector',
+    toDo: 'Select the knight in the fog',
+    selector: 'fog knight',
+    syntax: 'A  B',
+    help:
+    'Selects all <strong>B</strong> inside of <strong>A</strong>. <strong>B</strong> is called a descendant because it is inside of another element.',
+    examples: [
+      '<strong>p&nbsp;&nbsp;strong</strong> selects all <tag>strong</tag> elements that are inside of any <tag>p</tag>',
+      '<strong>#fancy&nbsp;&nbsp;span</strong> selects any <tag>span</tag> elements that are inside of the element with <strong>id="fancy"</strong>',
+    ],
+    htmlMarkup: `
+    <princess></princess>
+    <fog>
+      <knight></knight>
+    </fog>
+    <knight></knight>`
+  },
+  {
+    level: 5,
+    title: 'Combine the Descendant & ID Selectors',
+    toDo: 'Select knight in flower garden',
+    selector: '.flower knight',
+    syntax: '#id  A',
+    help:
+    'You can combine any selector with the descendent selector.',
+    examples: [
+      '<strong>#cool&nbsp;span</strong> selects all <tag>span</tag> elements that are inside of elements with <strong>id="cool"</strong>',
+    ],
+    htmlMarkup: `
+    <garden>
+      <princess></princess>
+    </garden>
+    <garden id="flower">
+      <knight></knight>
+    </garden>
+    <fortress>
+      <knight></knight>
+    </fortress>`
+  },
+  {
+    level: 6,
+    title: 'Class Selector',
+    toDo: 'Select the hidden enemies',
+    selector: '.hidden',
+    syntax: '.small',
+    help:
+    'The class selector selects all elements with that class attribute. Elements can only have one ID, but many classes.',
+    examples: [
+      '<strong>.neato</strong> selects all elements with <strong>class="neato"</strong>.',
+    ],
+    htmlMarkup: `
+    <knight></knight>
+    <princess></princess>
+    <knight class="hidden"></knight>
+    <assassin class="hidden"></assasin>`
+  },
+  {
+    level: 7,
+    title: '',
+    toDo: 'Select the hidden knights',
+    selector: 'knight.hidden',
+    syntax: 'A.className',
+    help:
+    'You can combine the class selector with other selectors, like the type selector.',
+    examples: [
+      '<strong>ul.important</strong> selects all <tag>ul</tag> elements that have <strong>class="important"</strong>',
+      '<strong>#big.wide</strong> selects all elements with <strong>id="big"</strong> that also have <strong>class="wide"</strong>',
+    ],
+    htmlMarkup: `
+    <assassin></assassin>
+    <assassin class="hidden"></assassin>
+    <fortress>
+      <knight class="hidden"></knight>
+    </fortress>
+    <garden>
+      <assassin></assassin>
+    </garden>
+    <garden>
+      <knight class="hidden"></knight>
+    </garden>`
+  },
+  {
+    level: 8,
+    title: '',
+    toDo: 'Select all hidden assassins in fog',
+    selector: 'fog assassin.hidden',
+    syntax: 'Put your back into it!',
+    help:
+    'Combine what you learned in the last few levels to solve this one!',
+    examples: [],
+    htmlMarkup: `
+    <fog><assassin></assasin></fog>
+    <assassin></assassin>
+    <fog><assassin class="hidden"></assassin></fog>
+    <garden><assassin class="hidden"></assassin></garden>`
+  },
+  {
+    level: 9,
+    title: 'Comma Combinator',
+    toDo: 'Select all the enemies',
+    selector: 'knight, assassin',
+    syntax: 'A, B',
+    help:
+    'Thanks to Shatner technology, this selects all <strong>A</strong> and <strong>B</strong> elements. You can combine any selectors this way, and you can specify more than two.',
+    examples: [
+      '<strong>p, .fun</strong> selects all <tag>p</tag> elements as well as all elements with <strong>class="fun"</strong>',
+            '<strong>a, p, div</strong> selects all <tag>a</tag>, <tag>p</tag> and <tag>div</tag> elements',
+    ],
+    htmlMarkup: `
+    <knight></knight>
+    <princess></princess>
+    <assassin></assassin>`
+  },
+  {
+    level: 10,
+    title: 'The Universal Selector',
+    toDo: 'Select all the things!',
+    selector: '*',
+    syntax: '*',
+    help:
+    'You can select all elements with the universal selector!',
+    examples: ['<strong>p *</strong> selects any element inside all <tag>p</tag> elements.'],
+    htmlMarkup: `
+    <fog>
+      <assassin class="hidden"></assassin>
+    </fog>
+    <dragon></dragon>
+    <princess></princess>
+    <garden>
+    <knight id="aggressive"></knight>
+    </garden>`
+  }
 ];
 
 export default levelsData;
